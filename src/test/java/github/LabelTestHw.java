21acf06bdeebc4595a80db96be9ffbd407a640d1
ironchupa <ic11.direct@gmail.com>
2022-07-27 15:46:28 +0300

    added HW test with listener

---
+++ src/test/java/github/LabelTestHw.java
@@ -0,0 +1,27 @@
+package github;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.conditions.Text;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class LabelTestHw {
+
+    @Test
+    public void labelTest () {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        open("https://github.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#issues-tab").click();
+        $(".subnav-links").shouldHave(Condition.text("Labels"));
+
+    }
+}
